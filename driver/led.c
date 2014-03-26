483551fe35299f2c28d152e39f2ed5728fd9932d
dingdongnigetou <dingdongnigetou@gmail.com>
2014-03-26 11:54:10 -0400

    led driver

---
+++ driver/led.c
@@ -0,0 +1,28 @@
+/*
+ * (C) 2014 JinDongHuang
+ *
+ * driver/led.c
+ *
+ * The implement of led driver device.
+ *
+ */
+
+#include "led.h"
+#include "s3c6410.h"
+
+void delay()
+{
+	int i, j;
+	for (i = 0;i < 50;i++)
+		for (j = 0;j < 100;j++);
+}
+
+void ledone_flicker()
+{
+	GPKCON0 = 0x00010000; 
+	GPKDAT  = 0;
+	delay();
+	GPKDAT  = 0xFFFFFFFF;
+	delay();
+}
+
