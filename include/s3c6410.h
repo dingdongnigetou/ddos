d1204ae497af22dbd450a9521b351044d4f40650
dingdongnigetou <dingdongnigetou@gmail.com>
2014-03-27 11:15:25 -0400

    Add some clock resgisters

--- include/s3c6410.h
+++ include/s3c6410.h
@@ -29,3 +29,13 @@
 /* Watch Dog */
 #define WTCON       (*(volatile unsigned long *)0x7E004000)
 
+/* clock */
+#define APLL_LOCK   (*(volatile unsigned long *)0x7E00F000)
+#define MPLL_LOCK   (*(volatile unsigned long *)0x7E00F004)
+#define EPLL_LOCK   (*(volatile unsigned long *)0x7E00F008)
+#define APLL_CON    (*(volatile unsigned long *)0x7E00F00C)
+#define MPLL_CON    (*(volatile unsigned long *)0x7E00F010)
+#define CLK_SRC     (*(volatile unsigned long *)0x7E00F01C)
+#define CLK_DIV0    (*(volatile unsigned long *)0x7E00F020)
+#define OTHERS    (*((volatile unsigned long *)0x7E00F900))
+
