53e1884b3bebdbdbeb0492ed4cfcfa0c611ae118
dingdongnigetou <dingdongnigetou@gmail.com>
2014-03-27 11:15:01 -0400

    Add keyborad.h

---
+++ include/keyboard.h
@@ -0,0 +1,19 @@
+/*
+ * (C) 2014 JinDongHuang
+ *
+ * include/keyboard.h
+ *
+ * Include some key code.
+ *
+ */
+
+#ifndef _KEYBOARD_H
+#define _KEYBOARD_H
+
+#define BACKSPACE 8
+#define NEWLINE   10
+#define RETURN    13
+#define SPACE     32
+
+#endif /* _KEYBOARD_H */
+
