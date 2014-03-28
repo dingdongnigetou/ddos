7bd9d8b8637b9e5dfaf4c10e38465eafdb9f58fc
dingdongnigetou <dingdongnigetou@gmail.com>
2014-03-28 17:29:50 -0400

    lcd driver initialization

---
+++ driver/lcd.c
@@ -0,0 +1,17 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * driver/lcd.c
+ *
+ * The implementation of lcd driver. 
+ *
+ */
+
+#include "lcd.h"
+#include "s3c6410"
+
+void lcd_init()
+{
+
+}
+
---
+++ include/lcd.h
@@ -0,0 +1,17 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * include/lcd.h
+ *
+ * interface of lcd driver
+ *
+ */
+
+#ifndef _LCD_H
+#define _LCD_H
+
+/* Initialize lcd */
+void lcd_init();
+
+#endif /* _LCD_H */
+
