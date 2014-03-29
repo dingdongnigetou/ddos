24f17eda6c1714067a9ad684b3228422aa241c04
dingdongnigetou <dingdongnigetou@gmail.com>
2014-03-29 11:23:21 -0400

    back light driver

---
+++ include/bglight.h
@@ -0,0 +1,19 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * include/bglight.h
+ *
+ * The interface of bglight.c.
+ * You should start back ground light first 
+ * if you want to control LCD.
+ *
+ */
+
+#ifndef _BGLIGHT_H_
+#define _BGLIGHT_H_
+
+/* set the background light of LCD with brightness */
+void set_bglight(unsigned brightness);
+
+#endif /* _BGLIGHT_H_ */
+
