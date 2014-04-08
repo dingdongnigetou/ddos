e815ed352116752b3adf12b2eb977b7691759dbf
dingdongnigetou <dingdongnigetou@gmail.com>
2014-04-08 17:20:11 -0400

    add DRAM register

--- include/s3c6410.h
+++ include/s3c6410.h
@@ -7,6 +7,9 @@
  *
  */
 
+#ifndef _S3C6410_H_
+#define _S3C6410_H_
+
 /* GPIO */
 #define GPACON         (*(volatile unsigned long *)0x7F008000)     
 #define GPADAT         (*(volatile unsigned long *)0x7F008004)
@@ -73,6 +76,34 @@
 #define TCNTB3         (*(volatile unsigned long *)0x7F006030)
 #define TINT_CSTAT     (*(volatile unsigned long *)0x7F006044)
 
+/* DRAM */
+#define MEMCCMD	       (*(volatile unsigned long *)0x7E001004)
+#define P1REFRESH      (*(volatile unsigned long *)0x7E001010)
+#define P1CASLAT       (*(volatile unsigned long *)0x7E001014) 
+#define MEM_SYS_CFG    (*(volatile unsigned long *)0x7E00F120)
+#define P1MEMCFG       (*(volatile unsigned long *)0x7E00100C)
+#define P1T_DQSS       (*(volatile unsigned long *)0x7E001018)
+#define P1T_MRD	       (*(volatile unsigned long *)0x7E00101C)
+#define P1T_RAS	       (*(volatile unsigned long *)0x7E001020)
+#define P1T_RC	       (*(volatile unsigned long *)0x7E001024)
+#define P1T_RCD	       (*(volatile unsigned long *)0x7E001028)
+#define P1T_RFC	       (*(volatile unsigned long *)0x7E00102C)
+#define P1T_RP	       (*(volatile unsigned long *)0x7E001030)
+#define P1T_RRD	       (*(volatile unsigned long *)0x7E001034)
+#define P1T_WR	       (*(volatile unsigned long *)0x7E001038)
+#define P1T_WTR	       (*(volatile unsigned long *)0x7E00103C)
+#define P1T_XP	       (*(volatile unsigned long *)0x7E001040)
+#define P1T_XSR	       (*(volatile unsigned long *)0x7E001044)
+#define P1T_ESR	       (*(volatile unsigned long *)0x7E001048)
+#define P1MEMCFG2      (*(volatile unsigned long *)0X7E00104C)
+#define P1_chip_0_cfg  (*(volatile unsigned long *)0x7E001200)
+
+#define P1MEMSTAT      (*(volatile unsigned long *)0x7E001000)
+#define P1MEMCCMD      (*(volatile unsigned long *)0x7E001004)
+#define P1DIRECTCMD    (*(volatile unsigned long *)0x7E001008)
+
 /* Others */
-#define PCLK           66500000 
+#define PCLK            66500000 
+
+#endif /* _S3C6410_H_ */
 
