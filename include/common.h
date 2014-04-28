bc4157ee2947da93e8ae2bbe88e21338279879e6
dingdongnigetou <dingdongnigetou@gmail.com>
2014-04-27 21:14:04 -0400

    common define

---
+++ include/common.h
@@ -0,0 +1,22 @@
+/*
+ * (C) Jindong Huang
+ *
+ * include/common.h
+ *
+ * Some defines that use commonly
+ *
+ */
+
+#ifndef _COMMON_H 
+#define _COMMON_H
+
+/* key code */
+#define BACKSPACE 8
+#define NEWLINE   10
+#define RETURN    13
+#define SPACE     32
+
+#define NULL (void *)0
+
+#endif /* _COMMON_H */
+
