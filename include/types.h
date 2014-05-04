9a8ee051582130b8adaa68f71d2085050ce9d699
dingdongnigetou <dingdongnigetou@gmail.com>
2014-05-04 17:29:48 -0400

    typedef some types

---
+++ include/types.h
@@ -0,0 +1,20 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * include/types.h
+ *
+ * Typedef of define some types.
+ *
+ */
+
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef unsigned int   size_t;
+
+typedef unsigned char  u_char;
+typedef unsigned long  u_long;
+typedef unsigned int   u_int;
+
+#endif /* _TYPES_H */
+
