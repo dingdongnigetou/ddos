3ad3e3397bbe65b11c65f26974e9f000c28669c5
dingdongnigetou <dingdongnigetou@gmail.com>
2014-05-18 11:37:03 -0400

    MMU

---
+++ mm/mmu.c
@@ -0,0 +1,72 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * mm/mmu.c
+ *
+ * initialize the MMU
+ * 
+ */
+
+#define REGISTER_USE_CACHE    0
+#define MMU_BASE              0x50360000
+
+#define MMU_FULL_ACCESS       (3 << 10)  /* access permission */
+#define MMU_DOMAIN            (0 << 5)   /* which domain */
+#define MMU_SPECIAL           (1 << 4)   /* bit 4 must be 1 */
+#define MMU_CACHEABLE         (1 << 3)   /* enable cache */
+#define MMU_BUFFERABLE        (1 << 2)   /* enable write buffer */
+#define MMU_SECDESC           (2)        /* segment descriptor */
+
+#define MMU_SECDESC_WB        (MMU_FULL_ACCESS | MMU_DOMAIN | \
+	                       MMU_SPECIAL | MMU_CACHEABLE |  \
+	                       MMU_BUFFERABLE | MMU_SECDESC)
+
+#define MMU_SECDESC_NCNB      (MMU_FULL_ACCESS | MMU_DOMAIN | \
+	                       MMU_SPECIAL | MMU_SECDESC)
+
+/*
+ *  To enable the MMU:
+ *  1) Program the translation table base and domain access control registers.
+ *  2) Program level 1 and level 2 page tables as required.
+ *  3) Enable the MMU by setting bit 0 in the control register.
+ *
+ */
+static void mmu_enable(unsigned long table)
+{
+__asm__ (
+
+	"mov r1, #0\n"
+	"mcr p15, 0, r1, c7, c7, 0\n" /* disable ICaches and ÍDCaches */
+	"mcr p15, 0, r1, c7, c10, 4\n"/* drain write buffer on v4 */
+	"mcr p15, 0, r1, c8, c7, 0\n" /* disable TLB */
+
+	"mcr p15, 0, %0, c2, c0, 0\n" /* write TTB register */
+	"mrc p15, 0, r1, c3, c0, 0\n" /* read domain 15:0 access permissions */
+	"orr r1, r1, #3\n"            /* domain 0, Accesses are not checked */
+	"mcr p15, 0, r1, c3, c0, 0\n" /* write domain 15:0 access permissions */
+	"mrc p15, 0, r1, c1, c0, 0\n" /* Read control register */
+	"orr r1, r1, #(1<<0)\n"       /* MMU enable */
+	"mcr p15,0,r1,c1, c0,0\n"     /* write control register */
+	:
+	: "r" (table)
+	: "r1"
+	);
+}
+
+void mmu_init(void)
+{
+	volatile unsigned long *table = (volatile unsigned long *)MMU_BASE;
+
+	/* 
+	 * build page table 
+	 *
+	 */
+
+	/* va: 0x00000000 => pa: 0x50000000*/
+	table[0x000] = 0x50000000 | MMU_SECDESC_WB;  
+	table[0x100] = 0x70000000 | MMU_SECDESC_WB;  	
+
+	mmu_enable((unsigned long)table);
+}
+
+
