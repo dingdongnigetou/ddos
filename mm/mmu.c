ba29ce9c463c17738a46c25d5b9ef4c9a6ddbf4d
dingdongnigetou <dingdongnigetou@gmail.com>
2014-05-27 21:48:47 -0400

    add second page

--- mm/mmu.c
+++ mm/mmu.c
@@ -23,18 +23,22 @@
 #define MMU_SPECIAL           (1 << 4)   /* bit 4 must be 1 */
 #define MMU_CACHEABLE         (1 << 3)   /* enable cache */
 #define MMU_BUFFERABLE        (1 << 2)   /* enable write buffer */
-#define MMU_SECDESC           (2)        /* segment descriptor */
+#define MMU_SEGMENT           (2)        /* segment descriptor */
+#define MMU_COARSE            (1)        /* coarse page table */
+#define MMU_SMALL             (2)        /* small page (second) */
 
 #define MMU_SECDESC_USER      (MMU_FULL_ACCESS | MMU_DOMAIN | \
-	                       MMU_SPECIAL | MMU_CACHEABLE |  \
-	                       MMU_BUFFERABLE | MMU_SECDESC)
+	                           MMU_SPECIAL | MMU_CACHEABLE |  \
+	                           MMU_BUFFERABLE | MMU_SECDESC)
 
-#define MMU_SECDESC_SVC       (MMU_SVC_ACCESS | MMU_DOMAIN | \
-	                       MMU_SPECIAL | MMU_CACHEABLE |  \
-	                       MMU_BUFFERABLE | MMU_SECDESC)
+#define MMU_SECDESC_SVC       (MMU_SVC_ACCESS | MMU_DOMAIN |  \
+	                           MMU_SPECIAL | MMU_CACHEABLE |  \
+	                           MMU_BUFFERABLE | MMU_SECDESC)
 
-#define MMU_SECDESC_NCNB      (MMU_FULL_ACCESS | MMU_DOMAIN | \
-	                       MMU_SPECIAL | MMU_SECDESC)
+#define MMU_SECDESC_USER_NCNB (MMU_FULL_ACCESS | MMU_DOMAIN | \
+	                            MMU_SPECIAL | MMU_SECDESC)
+
+#define MMU_COADESC           (MMU_DOMAIN | MMU_SPECIAL | MMU_COARSE)
 
 /*
  *  To enable the MMU:
@@ -58,8 +62,8 @@ __asm__ (
 	"mcr p15, 0, r1, c3, c0, 0\n" /* write domain 15:0 access permissions */
 
 	"mrc p15, 0, r1, c1, c0, 0\n" /* Read control register */
-	"orr r1, r1, #(1<<2)\n"       /* Data cache enable */
-	"orr r1, r1, #(1<<12)\n"      /* Instruction cache enable */
+	"orr r1, r1, #(1<<2)\n"       /* Dcache enable */
+	"orr r1, r1, #(1<<12)\n"      /* Icache enable */
 	"orr r1, r1, #(1<<14)\n"      /* Round robin replacement */
 	"orr r1, r1, #(1<<0)\n"       /* MMU enable */
 	"mcr p15,0,r1,c1, c0,0\n"     /* write control register */
@@ -75,22 +79,25 @@ static void memory_map(volatile unsigned long *table)
 	int i;
 
 	/* va: 0x00000000 => pa: 0 */
-	table[0x000] = 0 | MMU_SECDESC_USER;  
+	table[0x000] = 0 | MMU_COADESC;  
 
 	/* 
 	 * 256M peripheral
 	 * va: 0x10000000~0x1FF00000 => 0x70000000 ~ 0x7FF00000
 	 */
 	for (i = 0; i < 256; i++)
-		table[0x100 + i] = (0x70000000 + (i << 20)) | MMU_SECDESC_NCNB;
+		table[0x100 + i] = (0x70000000 + (i << 20)) | MMU_COADESC;
 
-	table[0xC00] = 0x50000000 | MMU_SECDESC_USER;
-	table[0xC01] = 0x50100000 | MMU_SECDESC_USER;
-	table[0xC02] = 0x50200000 | MMU_SECDESC_USER;
-	table[0xC03] = 0x50300000 | MMU_SECDESC_USER;
-	table[0xC04] = 0x50400000 | MMU_SECDESC_USER;
-	table[0xC05] = 0x50500000 | MMU_SECDESC_USER;
-	table[0xCF0] = 0x5F000000 | MMU_SECDESC_USER;
+	table[0xC00] = 0x50000000 | MMU_COADESC;
+	table[0xC01] = 0x50100000 | MMU_COADESC;
+	table[0xC02] = 0x50200000 | MMU_COADESC;
+	table[0xC03] = 0x50300000 | MMU_COADESC;
+	table[0xC04] = 0x50400000 | MMU_COADESC;
+	table[0xC05] = 0x50500000 | MMU_COADESC;
+	table[0xCF0] = 0x5F000000 | MMU_COADESC;
+
+//	for (i = 0; i < 256; i++)
+//		table[0x00 + i] = (0x00 + i) | MMU_SMALL_USER;
 }
 
 void mmu_init(void)
