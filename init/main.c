f41ea60ef96deaa58a286b02b6ba0178c9ba28fd
dingdongnigetou <dingdongnigetou@gmail.com>
2014-06-02 07:50:22 -0400

    enbale L1 and L2 table

--- init/main.c
+++ init/main.c
@@ -17,6 +17,8 @@ int main()
 {	
 	char c;
 
+	lcd_init();
+
 	while (1){
 		c = uart_getc();
 		uart_putc(c);
--- mm/mmu.c
+++ mm/mmu.c
@@ -13,6 +13,7 @@
  * per page is 4K -> total is 4G
  *
  */
+#define ENABLE_SEMGMENT 1
 
 #ifdef ENABLE_SEMGMENT
 /*
@@ -23,22 +24,18 @@
  * [3] C [2] B [1:0] is 2
  *
  */
-#define MMU_BASE              0x50600000
-#define MMU_SVC_ACCESS        (0 << 10)  /* only svc acsess */
-#define MMU_FULL_ACCESS       (3 << 10)  /* access permission */
+#define MMU_BASE              0x51000000
+#define MMU_SVC_ACCESS        (0 << 10)  /* only svc accsess */
+#define MMU_FULL_ACCESS       (3 << 10)  /* all can access */
 #define MMU_DOMAIN            (0 << 5)   /* which domain */
 #define MMU_SPECIAL           (1 << 4)   /* bit 4 must be 1 */
-#define MMU_CACHEABLE         (1 << 3)   /* enable cache */
-#define MMU_BUFFERABLE        (1 << 2)   /* enable write buffer */
+#define MMU_WT                (0x2 << 2) /* write through */
+#define MMU_WB                (0x3 << 2) /* write back */
 #define MMU_SEGMENT           (2)        /* segment descriptor */
 
 #define MMU_SECDESC           (MMU_FULL_ACCESS | MMU_DOMAIN | \
-	                       MMU_SPECIAL | MMU_CACHEABLE |  \
-	                       MMU_BUFFERABLE | MMU_SEGMENT)
+	                       MMU_SPECIAL | MMU_WB | MMU_SEGMENT)
 
-#define MMU_SVC               (MMU_SVC_ACCESS | MMU_DOMAIN | \
-	                       MMU_SPECIAL | MMU_CACHEABLE |  \
-	                       MMU_BUFFERABLE | MMU_SEGMENT)
 #define SEG_TABLE             ((volatile unsigned long *)MMU_BASE)
 
 #else
@@ -58,7 +55,7 @@
  */
 
 /*
- * dir entry
+ * dir entry (16K)
  */
 #define MMU_DIR               0x50600000
 #define MMU_DOMAIN            (0 << 5)   /* domain 0 */
@@ -68,28 +65,32 @@
 /*
  * page table entry
  */
-#define MMU_PAGE              0x50604000
+#define MMU_PAGE              (MMU_DIR + (16 << 10))
 #define MMU_PAGE_N(N)         (MMU_PAGE + (N << 10))
-#define MMU_FULL_ACCESS       (0xFF << 4)/* access permission */
+#define MMU_FULL_ACCESS       (0xFF << 4)/* all can access */
 #define MMU_SVC_ACCESS        (0x00 << 4)/* only system can access */
-#define MMU_CACHEABLE         (1 << 3)   /* enable cache */
-#define MMU_BUFFERABLE        (1 << 2)   /* enable write buffer */
+#define MMU_WT                (0x2 << 2) /* write through */
+#define MMU_WB                (0x3 << 2) /* write back */
 #define MMU_SMALL             (2)        /* small page (second) */
+
+/*
+ * which page table 
+ */
+#define PAGE_0_BASE           (0)
+#define PAGE_PER_BASE         (1)
+#define PAGE_KERNEL_BASE      (PAGE_PER_BASE + 256)
 
 /*
  * descriptor flags
  */
 #define MMU_COADESC           (MMU_DOMAIN | MMU_SPECIAL | MMU_COARSE)
 
-#define MMU_SMALLDESC         (MMU_FULL_ACCESS | MMU_CACHEABLE | \
-			       MMU_BUFFERABLE  | MMU_SMALL)
+#define MMU_SMALLDESC         (MMU_FULL_ACCESS | MMU_WB | MMU_SMALL)
 
 #define DIR_TABLE             ((volatile unsigned long *)MMU_DIR)
 #define PAGE_TABLE_N(N)       ((volatile unsigned long *)MMU_PAGE_N(N))
 
 #endif /* ENABLE_SEMGMENT */
-
-
 
 #ifdef ENABLE_SEMGMENT
 /*
@@ -99,16 +100,17 @@ static void memory_map_seg()
 {
 	int i;
 
-	SEG_TABLE[0x000]  = 0 | MMU_SECDESC;
+	for (i = 0; i < 256; i++)
+		SEG_TABLE[0x000 + i] = (0x00000000 + (i << 20)) | MMU_SECDESC;
+
 	/*
 	 * 256M peripheral
 	 */
 	for (i = 0; i < 256; i++)
 		SEG_TABLE[0x100 + i] = (0x70000000 + (i << 20)) | MMU_SECDESC;
 
-	SEG_TABLE[0xC00] = 0x50000000 | MMU_SECDESC;
-	SEG_TABLE[0xC01] = 0x50100000 | MMU_SECDESC;
-	SEG_TABLE[0xC02] = 0x50200000 | MMU_SECDESC;
+	for (i = 0; i < 256; i++)
+		SEG_TABLE[0xC00 + i] = (0x50000000 + (i << 20)) | MMU_SECDESC;
 }
 
 #else
@@ -119,29 +121,36 @@ static void memory_map_L2()
 {
 	int i, j;
 
-	/* L1 -> dir entry */
-	DIR_TABLE[0xC00] = MMU_PAGE_N(0) | MMU_COADESC;
-	DIR_TABLE[0xC01] = MMU_PAGE_N(1) | MMU_COADESC;
-	DIR_TABLE[0xC02] = MMU_PAGE_N(2) | MMU_COADESC;
-	/* 
-	 * 256M peripheral
+	/*
+	 * L1 page entry
 	 */
+	DIR_TABLE[0x000] = MMU_PAGE_N(PAGE_0_BASE) | MMU_COADESC;
+	/* 256M peripheral */
 	for (i = 0; i < 256; i++)
-		DIR_TABLE[0x100 + i] = MMU_PAGE_N((i + 3)) | MMU_COADESC;
+		DIR_TABLE[0x100 + i] = MMU_PAGE_N((PAGE_PER_BASE + i)) |
+		       	MMU_COADESC;
+	/* kernel */
+	for (i = 0; i < 3; i++)
+		DIR_TABLE[0xC00 + i] = MMU_PAGE_N(PAGE_KERNEL_BASE + i) | 
+			MMU_COADESC;
 
-
-	/* L2 -> page table entry */
+	/*
+	 * L2 page entry
+	 */
+	for (j = 0; j < 256; j++)
+		PAGE_TABLE_N(PAGE_0_BASE)[0x00 + j] = (0x00000000 + 
+				(j << 12)) | MMU_SMALLDESC;
+	/* for peripheral */
+	for (i = 0; i < 256; i++)
+		for (j = 0; j < 256; j++)
+			PAGE_TABLE_N((PAGE_PER_BASE + i))[0x00 + j] = (0x70000000 + 
+					(i << 20) + (j << 12)) | MMU_SMALLDESC;
+	/* kernel */
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 256; j++)
-			PAGE_TABLE_N(i)[0x00 + j] = (0x50000000 + (i << 20) + 
-					(j << 12)) | MMU_SMALLDESC;
-	/*
-	 * for peripheral
-	 */
-	for (i = 0; i < 256; i++)
-		for (j = 0; j < 256; j++)
-			PAGE_TABLE_N((i + 3))[0x00 + j] = (0x70000000 + 
-					(i << 20) + (j << 12)) | MMU_SMALLDESC;
+			PAGE_TABLE_N(PAGE_KERNEL_BASE + i)[0x00 + j] = 
+				(0x50000000 + (i << 20) + (j << 12)) | MMU_SMALLDESC;
+
 }
 #endif /* ENABLE_SEMGMENT */
 
@@ -155,7 +164,6 @@ static void memory_map_L2()
 static void mmu_enable(unsigned long table)
 {
 __asm__ (
-
 	"mov r1, #0\n"
 	"mcr p15, 0, r1, c7, c7, 0\n" /* disable ICaches and ÍDCaches */
 	"mcr p15, 0, r1, c7, c10, 4\n"/* drain write buffer on v4 */
