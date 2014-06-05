a2c8dd0b52df571c4fde9b9e2f54c35001355e8b
dingdongnigetou <dingdongnigetou@gmail.com>
2014-06-04 22:06:16 -0400

    task managment

---
+++ include/task.h
@@ -0,0 +1,18 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * include/task.h
+ *
+ * structure tcb and interfaces on 
+ * control task
+ */
+
+#ifndef _TASK_H
+#define _TASK_H
+
+struct tcb{
+
+};
+
+#endif /* _TASK_H */
+
