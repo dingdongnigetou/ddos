d04230661ddcce771348f66e08b58a7c9502888c
dingdongnigetou <dingdongnigetou@gmail.com>
2014-06-07 12:10:40 -0400

    add some define to make readability

---
+++ include/led.h
@@ -0,0 +1,19 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * Some define of led
+ */
+
+#ifndef _LED_H
+#define _LED_H
+
+#define LED1 0
+#define LED2 1
+#define LED3 2
+#define LED4 3
+
+#define ON   1
+#define OFF  0
+
+#endif
+
