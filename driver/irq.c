99ab0bb6cedb584c60c85d057512aefe1a59c030
dingdongnigetou <dingdongnigetou@gmail.com>
2014-06-14 23:20:53 -0400

    initialize irq

--- driver/irq.c
+++ driver/irq.c
@@ -5,8 +5,22 @@
  *
  */
 
+#include <s3c6410.h>
+
+#define ENABLE_TIMER4 (1 << 28)
+#define ENABLE_KEYBD  (1 << 22)
+
+static void irq_init(void)
+{
+	VIC0INTENABLE &= ~(0xFFFFFFFF);
+	VIC0INTENABLE |= ENABLE_TIMER4;
+	VIC0INTENABLE |= ENABLE_KEYBD;
+}
+
 int sys_enirq()
 {
+	irq_init();
+
 	asm(                          
 		"mrs r0, spsr\n"
 		"bic r0, r0, #0x80\n" 
