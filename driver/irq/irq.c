34c510ab808997c9d390502b5bd58a22469fa32f
dingdongnigetou <dingdongnigetou@gmail.com>
2014-07-03 14:33:46 +0000

    add irq management

---
+++ driver/irq/irq.c
@@ -0,0 +1,92 @@
+/*
+ * (C) 2014 Jindong Huang
+ *
+ * driver/irq.c
+ *
+ */
+
+#include <s3c6410.h>
+
+/* VIC0INTENABLE */
+#define ENABLE_TIMER4 (1 << 28)
+#define ENABLE_EINT   (3 << 0)
+
+/* VIC0INTENCLEAR */
+#define CLEAR_TIMER4  (1 << 28)
+#define CLEAR_EINT    (3 << 0)
+
+/* clear int pending status */
+#define TIMER4_PENDING_CLEAR    (1 << 9)
+#define EINT_PENDING_CLEAR      (0x3F)
+
+void irq_init(void)
+{
+	GPNCON &= ~(0xfff);
+	GPNCON |= 0xaaa;
+
+	EINT0CON0 &= ~(0xfff);
+	EINT0CON0 |= 0x777;
+
+	/* enable eint0-6 : key 1-6 */
+	EINT0MASK &= ~(0x3f);
+
+	VIC0INTENABLE &= ~(0xFFFFFFFF);
+	VIC0INTENABLE |= ENABLE_TIMER4;
+	VIC0INTENABLE |= ENABLE_EINT;
+}
+
+int sys_enirq()
+{
+	asm(                          
+		"mrs r0, spsr\n"
+		"bic r0, r0, #0x80\n" 
+		"msr spsr, r0\n" 
+	   );
+
+	return 0;
+}
+
+int sys_disirq()
+{
+	asm(                     
+		"mrs r0, spsr\n"
+		"orr r0, r0, #0x80\n"
+		"msr spsr, r0\n"
+	   );
+
+	return 0;
+}
+
+void eint_process()
+{
+	/* key process */
+	int i;
+	for (i = 0; i < 6; i ++){
+		if (EINT0PEND & (1<<i)){
+			/* release */
+			if (GPNDAT & (1<<i))
+				;
+			/* press */
+			else
+				;
+		}
+	}
+
+	EINT0PEND = 0;
+	EINT0PEND |= EINT_PENDING_CLEAR;  /* clear int */
+	VIC0ADDRESS = 0;
+}
+
+/*
+ * It will call the schedule() in couple ms
+ * to preempt cpu.
+ */
+void sys_timer_process()
+{
+	puts("preempt");
+
+	TINT_CSTAT &= 0x1F;
+	TINT_CSTAT |= TIMER4_PENDING_CLEAR;
+	VIC0ADDRESS = 0;
+}
+
