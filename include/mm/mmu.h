18cda924abd2297d131e6ab942ea4a4d670c607b
dingdongnigetou <dingdongnigetou@gmail.com>
2014-08-05 20:55:20 +0800

    rewrite mmu

--- include/mm/mmu.h
+++ include/mm/mmu.h
@@ -11,7 +11,7 @@ typedef struct
 {
 	unsigned int va_addr;
 	unsigned int pt_addr;
-	unsigned int dir_addr;
+	unsigned int master_addr;
 	unsigned int type;
 	unsigned int dom;
 } Pagetable;
@@ -30,13 +30,51 @@ typedef struct
 /* L1 */
 #define FAULT    0
 #define COARSE   1
-#define SEGMENT  2
+#define MASTER   2
 #define FINE     3
 
 /* L2 */
 #define LAEGE    1
 #define SMALL    2
 #define TINY     3
+
+/* page size */
+#define LAEGEPAGE (64 << 10) /* 64KB */
+#define SMALLPAGE (4  << 10) /* 4KB */
+#define TINYPAGE  (1  << 10) /* 1KB */
+
+/* NA=not available RO=read only RW=read wirte */
+#define NANA 0x00
+#define RWNA 0x01
+#define RWRO 0x02
+#define RWRW 0x03
+
+/*
+ * cb = no cache and write buffer
+ * cB = no cache but write buffer
+ * WT = write throuh
+ * WB = write back
+ */
+#define cb   0x0
+#define cB   0x1
+#define WT   0x2
+#define WB   0x3
+
+/* page table */
+//Pagetable master_pt = {};
+//Pagetable system_pt  = {};
+//Pagetable task1_pt   = {};
+//Pagetable task2_pt   = {};
+//Pagetable task3_pt   = {};
+//
+///* region */
+//Region kernel_region = {};
+//Region shared_region = {};
+//Region dismem_region = {};
+//Region periph_region = {};
+//Region task1_region = {};
+//Region task2_region = {};
+//Region task3_region = {};
 
 #endif /* _MMU_H_ */
 
--- include/mm/ulities.h
+++ include/mm/ulities.h
@@ -10,25 +10,25 @@
 inline static void flush_tlb()
 {
 	unsigned int c8_format = 0;
-	_asm_(MCR, p15, 0, c8_format, c8, c7, 0)
+	__asm__(MCR, p15, 0, c8_format, c8, c7, 0);
 }
 
 inline static void flush_cache()
 {
 	unsigned int c7_format = 0;
-	_asm_(MCR, p15, 0, c7_format, c7, c7, 0)
+	__asm__(MCR, p15, 0, c7_format, c7, c7, 0);
 }
 
 inline static void flush_d_cache()
 {
 	unsigned int c7_format = 0;
-	_asm_(MCR, p15, 0, c7_format, c7, c6, 0)
+	__asm__(MCR, p15, 0, c7_format, c7, c6, 0);
 }
 
 inline static void flush_i_cache()
 {
 	unsigned int c7_format = 0;
-	_asm_(MCR, p15, 0, c7_format, c7, c5, 0)
+	__asm__(MCR, p15, 0, c7_format, c7, c5, 0);
 }
 
 
